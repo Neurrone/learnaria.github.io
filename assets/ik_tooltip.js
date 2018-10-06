7b10a50cb172833b25d20477a0c249632741ea2b
Dickson Tan <neurrone@users.noreply.github.com>
2018-10-06 18:21:03 +0800

    activity 6: fix tooltip

--- assets/ik_tooltip.js
+++ assets/ik_tooltip.js
@@ -38,14 +38,18 @@
 				.addClass('ik_tooltip')
 				.attr({
 					'id': id,
+					'role': 'tooltip',
+					'aria-hidden': 'true',
+					'aria-live': 'polite'
 				});
 			
 			$elem
+				.attr({tabindex: 0 })
 				.css('position', 'relative')
 				.removeAttr('title') // remove title to prevent it from being read
 				.after($tooltip)
-				.on('mouseover', function(event) {
-					
+				.on('mouseover focus', function(event) {
+console.log('mouseover or focus');					
 					var y, x;
 					
 					y = $elem.position().top - $tooltip.height() - 20;
@@ -62,6 +66,7 @@
 					}
 					
 					$tooltip // position and show tooltip
+						.attr({'aria-hidden': 'false' })
 						.css({
 							'top': y, 
 							'left': x
@@ -73,11 +78,27 @@
 					if (!$(event.currentTarget).is(':focus') ) { // hide tooltip if current element is not focused
 						
 						$tooltip
+							.attr({'aria-hidden': 'true'})
 							.removeClass('visible mouseover');					
 					}
 										
 				})
+				.on('blur', function(event) {               
+					if (!$tooltip.hasClass('mouseover') ) { // hide tooltip if mouse is not over the current element
+						$tooltip
+						.attr({'aria-hidden': 'true'})
+						.removeClass('visible');
+					}
+				})
+				.on('keyup', function(event) {          
+					if(event.keyCode == ik_utils.keys.esc) { // hide when escape key is pressed
+						$tooltip
+						.attr({'aria-hidden': 'true' })
+						.removeClass('visible');
+					}
+				});
 		}
+		
 	};
 	
 	$.fn[pluginName] = function ( options ) {
